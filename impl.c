4129652d8d186407f1e4edb0b93a8dd54d26056a
hugikun999 <hugikun999@outlook.com>
2017-03-17 01:18:24 +0800

    Add an implement base on navie_transpose

--- impl.c
+++ impl.c
@@ -8,6 +8,13 @@ void naive_transpose(int *src, int *dst, int w, int h)
             *(dst + x * h + y) = *(src + y * w + x);
 }
 
+void naive_transpose_reverse(int *src, int *dst, int w, int h)
+{
+    for (int x = 0; x < h; x++)
+        for (int y = 0; y < w; y++)
+            *(dst + y * h + x) = *(src + x * w + y);
+}
+
 void sse_transpose(int *src, int *dst, int w, int h)
 {
     for (int x = 0; x < w; x += 4) {
@@ -110,4 +117,33 @@ void sse_prefetch_transpose(int *src, int *dst, int w, int h)
     }
 }
 
+void sse_prefetch_transpose_dist(int *src, int *dst, int w, int h, int pfdist)
+{
+    for (int x = 0; x < w; x += 4) {
+        for (int y = 0; y < h; y += 4) {
+
+            _mm_prefetch(src+(y + pfdist + 0) *w + x, _MM_HINT_T1);
+            _mm_prefetch(src+(y + pfdist + 1) *w + x, _MM_HINT_T1);
+            _mm_prefetch(src+(y + pfdist + 2) *w + x, _MM_HINT_T1);
+            _mm_prefetch(src+(y + pfdist + 3) *w + x, _MM_HINT_T1);
+
+            __m128i I0 = _mm_loadu_si128 ((__m128i *)(src + (y + 0) * w + x));
+            __m128i I1 = _mm_loadu_si128 ((__m128i *)(src + (y + 1) * w + x));
+            __m128i I2 = _mm_loadu_si128 ((__m128i *)(src + (y + 2) * w + x));
+            __m128i I3 = _mm_loadu_si128 ((__m128i *)(src + (y + 3) * w + x));
+            __m128i T0 = _mm_unpacklo_epi32(I0, I1);
+            __m128i T1 = _mm_unpacklo_epi32(I2, I3);
+            __m128i T2 = _mm_unpackhi_epi32(I0, I1);
+            __m128i T3 = _mm_unpackhi_epi32(I2, I3);
+            I0 = _mm_unpacklo_epi64(T0, T1);
+            I1 = _mm_unpackhi_epi64(T0, T1);
+            I2 = _mm_unpacklo_epi64(T2, T3);
+            I3 = _mm_unpackhi_epi64(T2, T3);
+            _mm_storeu_si128((__m128i *)(dst + ((x + 0) * h) + y), I0);
+            _mm_storeu_si128((__m128i *)(dst + ((x + 1) * h) + y), I1);
+            _mm_storeu_si128((__m128i *)(dst + ((x + 2) * h) + y), I2);
+            _mm_storeu_si128((__m128i *)(dst + ((x + 3) * h) + y), I3);
+        }
+    }
+}
 #endif /* TRANSPOSE_IMPL */
